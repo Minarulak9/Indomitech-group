126b84bda4c667f0f46021f0cb29371afc51eaee
minarul <minarulak88@gmail.com>
2024-09-10 07:04:59 +0530

    Project add to github

---
+++ components/contact_us.php
@@ -0,0 +1,74 @@
+<section id="contact">
+    <div class="section-container">
+        <div class="contents">
+            <div class="contact-form">
+                <div class="section_heading">Contact Us</div>
+                <div class="primary_heading"><span class="text-light">Let's connect and</span> Grow your business</div>
+                <form method="POST" action="query_mail.php">
+                    <div class="input-div">
+                        <input type="text" name="name" required="" placeholder="Name">
+                    </div>
+                    <div class="input-div">
+                        <input type="text" name="phone" required="" placeholder="Phone">
+                    </div>
+                    <div class="input-div">
+                    </div>
+                    <div class="input-div">
+                        <input type="text" name="email" required="" placeholder="Email">
+                    </div>
+                    <div class="input-div">
+                        <select>
+                            <option value="null">Choose Requirement</option>
+                            <option value="Website">Website</option>
+                            <option value="App">App</option>
+                            <option value="Software">Software</option>
+                            <option value="Digital Marketing">Digital Marketing</option>
+                            <option value="Software Development">Software Development</option>
+                            <option value="UI/UX Design">UI/UX Design</option>
+                            <option value="Other">Other</option>
+                        </select>
+                    </div>
+                    <div class="input-div">
+                        <textarea required="" rows="6" name="message" placeholder="Message to us"></textarea>
+                    </div>
+                    <div class="btn-submit">
+                        <button type="submit" class="submit-btn">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <div class="details-card">
+                <iframe id="location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29445.77322327994!2d88.36704964543608!3d22.70140352277032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f89c0bf0119e61%3A0xf3dedf82eaf68307!2sSodepur%2C%20Kolkata%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1713608659607!5m2!1sen!2sin" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <!-- <div id="location"></div> -->
+                <div class="address_box">
+                    <div class="single-address">
+                        <div class="icon"><i class="fa-solid fa-location-dot"></i></div>
+                        <div class="address">
+                            Kolkata Office - Sodepur, Kolkata,
+                            <br>
+                            West-Bengal - 700110
+                            <br>
+                            Phone: <a class="text-inherit" href="tel:+91 98754 05470">+91 98754 05470</a>
+                        </div>
+                    </div>
+                    <div class="single-address">
+                        <div class="icon"><i class="fa-solid fa-location-dot"></i></div>
+                        <div class="address">
+                            Abu Dhabi Office - Electra Street, colors express building,
+                            <br>
+                            near life line hospital, Abu Dhabi UAE
+                            <br>
+                            Phone: <a class="text-inherit" href="tel:+971566447642">+971566447642</a>
+                        </div>
+                    </div>
+                    <!-- <div class="single-address">
+                            <div class="icon"><i class="fa-solid fa-location-dot"></i></div>
+                            <div class="address">
+                            SODEPUR Post Office is located at NORTH 24 PARGANAS, WEST BENGAL, 700110. NA Post Office is a Sub Post Office,
+                            </div>
+                        </div> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
---
+++ components/partners.php
@@ -0,0 +1,37 @@
+<section id="our_partners">
+        <div class="section-container">
+            <div class="section_heading">Our Partners</div>
+
+            <div class="partner-slider">
+                <div class="swiper partner_swiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="img">
+                                <img src="./img/partners/yolopment.jpeg" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="img">
+                                <img src="./img/partners/syscogen.png" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="img">
+                                <img src="./img/partners/dishari.png" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="img">
+                                <img src="./img/partners/gopotu.jpeg" alt="">
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="img">
+                                <img src="./img/partners/futurebuzz.webp" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
